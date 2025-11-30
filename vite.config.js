14e098c28e8dd9c1a7427612b2c9f25f16fc3b89
Michael Yarichuk <michaely@bindecy.com>
2025-11-30 22:43:48 +0200

    adjust config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: change "elena-portfolio" → your repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/elena-portfolio/', 
+  base: '/', 
 })
